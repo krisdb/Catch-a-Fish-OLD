8d60d78744e6b55b60e0570f3c8fab7181efb0d0
krisdb <kris@qualcode.net>
2016-04-23 20:18:35 -0400

    connections

--- app/src/main/java/com/qualcode/catchafish/MainActivity.java
+++ app/src/main/java/com/qualcode/catchafish/MainActivity.java
@@ -1,61 +1,153 @@
 package com.qualcode.catchafish;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.iid.InstanceID;
 import com.google.android.gms.nearby.Nearby;
-import com.google.android.gms.nearby.connection.AppIdentifier;
-import com.google.android.gms.nearby.connection.AppMetadata;
-import com.google.android.gms.nearby.connection.Connections;
-import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
-import com.qualcode.catchafish.R;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+import com.google.android.gms.nearby.messages.NearbyMessagesStatusCodes;
+import com.google.android.gms.nearby.messages.PublishCallback;
+import com.google.android.gms.nearby.messages.PublishOptions;
+import com.google.android.gms.nearby.messages.Strategy;
+import com.google.android.gms.nearby.messages.SubscribeCallback;
+import com.google.android.gms.nearby.messages.SubscribeOptions;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        Connections.ConnectionRequestListener,
-        Connections.EndpointDiscoveryListener,
-        Connections.MessageListener,
         View.OnClickListener
 {
 
-    private static GoogleApiClient mGoogleApiClient;
-    private MyListDialog mMyListDialog;
-    private AlertDialog mConnectionRequestDialog;
-    private static final long TIMEOUT_ADVERTISE = 1000L * 30L;
-    private static final long TIMEOUT_DISCOVER = 1000L * 30L;
-    private String mOtherEndpointId;
+    private static final String TAG = "MainActivity";
+
+    private static final Strategy PUB_SUB_STRATEGY = new Strategy.Builder() .setTtlSeconds(3 * 60).build();
+    private GoogleApiClient mGoogleApiClient;
+
+    private Message mDeviceInfoMessage;
+
+    private MessageListener mMessageListener;
+
+    private boolean mResolvingNearbyPermissionError = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn_discover).setOnClickListener(this);
-        findViewById(R.id.btn_advertise).setOnClickListener(this);
+        findViewById(R.id.btn_available).setOnClickListener(this);
+        findViewById(R.id.btn_catch).setOnClickListener(this);
+
+        mDeviceInfoMessage = DeviceMessage.newNearbyMessage(InstanceID.getInstance(getApplicationContext()).getId());
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Nearby.MESSAGES_API)
-                .addApi(Nearby.CONNECTIONS_API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+
+        mMessageListener = new MessageListener() {
+            @Override
+            public void onFound(final Message message) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ((TextView)findViewById(R.id.txt_status)).setText(DeviceMessage.fromNearbyMessage(message).getMessageBody());
+                    }
+                });
+            }
+
+            @Override
+            public void onLost(final Message message) {
+            }
+        };
     }
+
+    private void publish() {
+        ((Button)findViewById(R.id.btn_available)).setText("Unavailable");
+        if (!mGoogleApiClient.isConnected()) {
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        } else {
+            PublishOptions options = new PublishOptions.Builder()
+                    .setStrategy(PUB_SUB_STRATEGY)
+                    .setCallback(new PublishCallback() {
+                        @Override
+                        public void onExpired() {
+                            super.onExpired();
+                        }
+                    }).build();
+
+            Nearby.Messages.publish(mGoogleApiClient, mDeviceInfoMessage, options)
+                    .setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status status) {
+                            if (status.isSuccess()) {
+                                //display publishing message
+                            } else {
+                                handleUnsuccessfulNearbyResult(status);
+                            }
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Subscribes to messages from nearby devices. If not successful, attempts to resolve any error
+     * related to Nearby permissions by displaying an opt-in dialog. Registers a callback which
+     * updates state when the subscription expires.
+     */
+    private void subscribe() {
+        ((Button)findViewById(R.id.btn_catch)).setText("Stop Fishing");
+
+        if (!mGoogleApiClient.isConnected()) {
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        } else {
+            SubscribeOptions options = new SubscribeOptions.Builder()
+                    .setStrategy(PUB_SUB_STRATEGY)
+                    .setCallback(new SubscribeCallback() {
+                        @Override
+                        public void onExpired() {
+                            super.onExpired();
+                            Log.i(TAG, "no longer subscribing");
+                        }
+                    }).build();
+
+            Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener, options)
+                    .setResultCallback(new ResultCallback<Status>() {
+
+                        @Override
+                        public void onResult(Status status) {
+                            if (status.isSuccess()) {
+                                Log.i(TAG, "subscribed successfully");
+                            } else {
+                                Log.i(TAG, "could not subscribe");
+                                handleUnsuccessfulNearbyResult(status);
+                            }
+                        }
+                    });
+        }
+    }
+
 
     @Override
     public void onStart() {
@@ -68,194 +160,64 @@ public class MainActivity extends AppCompatActivity implements
     public void onStop() {
         super.onStop();
 
+        Nearby.Messages.unpublish(mGoogleApiClient, mDeviceInfoMessage);
+        ((Button)findViewById(R.id.btn_available)).setText("I'm Available");
         if (mGoogleApiClient != null) {
             mGoogleApiClient.disconnect();
         }
     }
 
-    private void startAdvertising() {
-
-        // Advertising with an AppIdentifer lets other devices on the network discover
-        // this application and prompt the user to install the application.
-        List<AppIdentifier> appIdentifierList = new ArrayList<>();
-        appIdentifierList.add(new AppIdentifier(getPackageName()));
-        AppMetadata appMetadata = new AppMetadata(appIdentifierList);
-
-        // Advertise for Nearby Connections. This will broadcast the service id defined in
-        // AndroidManifest.xml. By passing 'null' for the name, the Nearby Connections API
-        // will construct a default name based on device model such as 'LGE Nexus 5'.
-        String name = null;
-        Nearby.Connections.startAdvertising(mGoogleApiClient, name, appMetadata, TIMEOUT_ADVERTISE,
-                this).setResultCallback(new ResultCallback<Connections.StartAdvertisingResult>() {
-            @Override
-            public void onResult(Connections.StartAdvertisingResult result) {
-                if (result.getStatus().isSuccess()) {
-                    ((TextView)findViewById(R.id.txt_status)).setText("Advertising...");
-                } else {
-
-                    // If the user hits 'Advertise' multiple times in the timeout window,
-                    // the error will be STATUS_ALREADY_ADVERTISING
-                    int statusCode = result.getStatus().getStatusCode();
-                    if (statusCode == ConnectionsStatusCodes.STATUS_ALREADY_ADVERTISING) {
-                        //debugLog("STATUS_ALREADY_ADVERTISING");
-                    } else {
-                    }
-                }
-            }
-        });
-    }
-
-
-    private void startDiscovery() {
-
-        // Discover nearby apps that are advertising with the required service ID.
-        String serviceId = getString(R.string.service_id);
-        Nearby.Connections.startDiscovery(mGoogleApiClient, serviceId, TIMEOUT_DISCOVER, this).setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(Status status) {
-                if (status.isSuccess()) {
-                    ((TextView)findViewById(R.id.txt_status)).setText("Searching...");
-                } else {
-
-                    // If the user hits 'Discover' multiple times in the timeout window,
-                    // the error will be STATUS_ALREADY_DISCOVERING
-                    int statusCode = status.getStatusCode();
-                    if (statusCode == ConnectionsStatusCodes.STATUS_ALREADY_DISCOVERING) {
-                        //debugLog("STATUS_ALREADY_DISCOVERING");
-                    } else {
-                        //updateViewVisibility(STATE_READY);
-                    }
-                }
-            }
-        });
-    }
-
-
     @Override
-    public void onEndpointFound(final String endpointId, String deviceId, String serviceId, final String endpointName) {
+    public void onConnected(Bundle connectionHint) {
 
-        // This device is discovering endpoints and has located an advertiser. Display a dialog to
-        // the user asking if they want to connect, and send a connection request if they do.
-        if (mMyListDialog == null) {
-            // Configure the AlertDialog that the MyListDialog wraps
-            AlertDialog.Builder builder = new AlertDialog.Builder(this)
-                    .setTitle("Endpoint(s) Found")
-                    .setCancelable(true)
-                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            mMyListDialog.dismiss();
-                        }
-                    });
+    }
 
-            // Create the MyListDialog with a listener
-            mMyListDialog = new MyListDialog(this, builder, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    String selectedEndpointName = mMyListDialog.getItemKey(which);
-                    String selectedEndpointId = mMyListDialog.getItemValue(which);
+    private void handleUnsuccessfulNearbyResult(Status status) {
+        Log.i(TAG, "processing error, status = " + status);
+        if (status.getStatusCode() == NearbyMessagesStatusCodes.APP_NOT_OPTED_IN) {
+            if (!mResolvingNearbyPermissionError) {
+                try {
+                    mResolvingNearbyPermissionError = true;
+                    status.startResolutionForResult(this, Constants.REQUEST_RESOLVE_ERROR);
 
-                    MainActivity.this.connectTo(selectedEndpointId, selectedEndpointName);
-                    mMyListDialog.dismiss();
+                } catch (IntentSender.SendIntentException e) {
+                    e.printStackTrace();
                 }
-            });
+            }
+        } else {
+            if (status.getStatusCode() == ConnectionResult.NETWORK_ERROR) {
+                Toast.makeText(this,
+                        "No connectivity, cannot proceed. Fix in 'Settings' and try again.",
+                        Toast.LENGTH_LONG).show();
+                //resetToDefaultState();
+            } else {
+                // To keep things simple, pop a toast for all other error messages.
+                Toast.makeText(this, "Unsuccessful: " +
+                        status.getStatusMessage(), Toast.LENGTH_LONG).show();
+            }
+
         }
-
-        mMyListDialog.addItem(endpointName, endpointId);
-        mMyListDialog.show();
     }
 
-    private void connectTo(String endpointId, final String endpointName) {
-        final String myName = null;
-        byte[] myPayload = null;
-
-        Nearby.Connections.sendConnectionRequest(mGoogleApiClient, myName, endpointId, myPayload,
-                new Connections.ConnectionResponseCallback() {
-                    @Override
-                    public void onConnectionResponse(String endpointId, Status status, byte[] bytes) {
-                        if (status.isSuccess()) {
-                            ((TextView)findViewById(R.id.txt_status)).setText("Connected to: " + endpointName);
-
-                        } else {
-                            //debugLog("onConnectionResponse: " + endpointName + " FAILURE");
-                        }
-                    }
-                }, this);
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "connection to GoogleApiClient failed");
     }
 
 
     @Override
-    public void onEndpointLost(String endpointId) {
-    }
-
-    @Override
-    public void onConnectionRequest(final String endpointId, String deviceId, final String endpointName, byte[] payload) {
-
-        // This device is advertising and has received a connection request. Show a dialog asking
-        // the user if they would like to connect and accept or reject the request accordingly.
-        mConnectionRequestDialog = new AlertDialog.Builder(this)
-                .setTitle("Connection Request")
-                .setMessage("Do you want to connect to " + endpointName + "?")
-                .setCancelable(false)
-                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        byte[] payload = null;
-                        Nearby.Connections.acceptConnectionRequest(mGoogleApiClient, endpointId,
-                                payload, MainActivity.this)
-                                .setResultCallback(new ResultCallback<Status>() {
-                                    @Override
-                                    public void onResult(Status status) {
-                                        if (status.isSuccess()) {
-                                            mOtherEndpointId = endpointId;
-                                            ((TextView)findViewById(R.id.txt_status)).setText("Connected to: " + endpointName);
-                                        } else {
-                                            //debugLog("acceptConnectionRequest: FAILURE");
-                                        }
-                                    }
-                                });
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Nearby.Connections.rejectConnectionRequest(mGoogleApiClient, endpointId);
-                    }
-                }).create();
-
-        mConnectionRequestDialog.show();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-            }
-
-    @Override
-    public void onDisconnected(String endpointId) {
-    }
-
-    @Override
-    public void onMessageReceived(String endpointId, byte[] payload, boolean isReliable) {
-        // A message has been received from a remote endpoint.
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        mGoogleApiClient.reconnect();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "GoogleApiClient connection suspended: " + cause);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.btn_advertise:
-                startAdvertising();
+            case R.id.btn_available:
+                publish();
                 break;
-            case R.id.btn_discover:
-                startDiscovery();
+            case R.id.btn_catch:
+                subscribe();
                 break;
         }
     }
